fe218c1ebc08ece76b744857a242f71ef1a7a209
valdersonjr <valdersonpontes@gmail.com>
2023-11-20 20:18:23 -0300

    fix exercicio 2

--- Exercicios/Exercicio_2/exemploExercicio2.ts
+++ Exercicios/Exercicio_2/exemploExercicio2.ts
@@ -1,10 +1,5 @@
 import { findMedianSortedArrays } from "./mediana2ArrayOrdenados";
 
-// Testando com os exemplos fornecidos
-const nums1Exemplo1 = [1, 3];
-const nums2Exemplo1 = [2];
-console.log(findMedianSortedArrays(nums1Exemplo1, nums2Exemplo1)); // Saída: 2
-
-const nums1Exemplo2 = [1, 2];
-const nums2Exemplo2 = [3, 4];
-console.log(findMedianSortedArrays(nums1Exemplo2, nums2Exemplo2)); // Saída: 2.5
+// Exemplos de uso
+console.log(findMedianSortedArrays([1, 3], [2])); // Saída: 2.00000
+console.log(findMedianSortedArrays([1, 2], [3, 4])); // Saída: 2.50000
--- Exercicios/Exercicio_2/mediana2ArrayOrdenados.ts
+++ Exercicios/Exercicio_2/mediana2ArrayOrdenados.ts
@@ -1,110 +1,46 @@
-// Função principal para encontrar a mediana de dois arrays ordenados
 export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    // Função para mesclar dois arrays ordenados e encontrar a mediana
-    const mesclarEMediana = (arr1: number[], arr2: number[]): number => {
-        const arrayMesclado = mesclarArrays(arr1, arr2);
-        const comprimentoMesclado = arrayMesclado.length;
+    const tamanhoTotal = nums1.length + nums2.length;
 
-        if (comprimentoMesclado % 2 === 0) {
-            const meioDireita = comprimentoMesclado / 2;
-            const meioEsquerda = meioDireita - 1;
-            return (arrayMesclado[meioEsquerda] + arrayMesclado[meioDireita]) / 2;
-        } else {
-            const meio = Math.floor(comprimentoMesclado / 2);
-            return arrayMesclado[meio];
-        }
-    };
-
-    // Função para mesclar dois arrays ordenados
-    const mesclarArrays = (arr1: number[], arr2: number[]): number[] => {
-        const mesclado:number[] = [];
-        let i = 0;
-        let j = 0;
-
-        // Comparação de elementos dos dois arrays
-        while (i < arr1.length && j < arr2.length) {
-            if (arr1[i] < arr2[j]) {
-                mesclado.push(arr1[i]);
-                i++;
-            } else {
-                mesclado.push(arr2[j]);
-                j++;
-            }
-        }
-
-        // Adiciona elementos restantes de arr1
-        while (i < arr1.length) {
-            mesclado.push(arr1[i]);
-            i++;
-        }
-
-        // Adiciona elementos restantes de arr2
-        while (j < arr2.length) {
-            mesclado.push(arr2[j]);
-            j++;
-        }
-
-        return mesclado;
-    };
-
-    // Função recursiva para encontrar a mediana
-    const encontrarMedianaRecursivamente = (
-        arr1: number[],
-        inicio1: number,
-        fim1: number,
-        arr2: number[],
-        inicio2: number,
-        fim2: number
-    ): number => {
-        // Caso base para arrays pequenos
-        if (fim1 - inicio1 <= 1 || fim2 - inicio2 <= 1) {
-            const mesclado = mesclarArrays(
-                arr1.slice(inicio1, fim1 + 1),
-                arr2.slice(inicio2, fim2 + 1)
-            );
-            return mesclarEMediana(arr1.slice(inicio1, fim1 + 1), arr2.slice(inicio2, fim2 + 1));
-        }
-
-        // Encontra as medianas dos arrays
-        const mediana1 = encontrarMediana(arr1, inicio1, fim1);
-        const mediana2 = encontrarMediana(arr2, inicio2, fim2);
-
-        // Compara as medianas
-        if (mediana1 === mediana2) {
-            return mediana1;
-        } else if (mediana1 < mediana2) {
-            const meio1 = inicio1 + Math.floor((fim1 - inicio1) / 2);
-            const meio2 = inicio2 + Math.floor((fim2 - inicio2) / 2);
-            return encontrarMedianaRecursivamente(arr1, meio1, fim1, arr2, inicio2, meio2);
-        } else {
-            const meio1 = inicio1 + Math.floor((fim1 - inicio1) / 2);
-            const meio2 = inicio2 + Math.floor((fim2 - inicio2) / 2);
-            return encontrarMedianaRecursivamente(arr1, inicio1, meio1, arr2, meio2, fim2);
-        }
-    };
-
-    // Função para encontrar a mediana de um array
-    const encontrarMediana = (arr: number[], inicio: number, fim: number): number => {
-        const n = fim - inicio + 1;
-        const meio = inicio + Math.floor(n / 2);
-
-        if (n % 2 === 0) {
-            const meioEsquerda = meio - 1;
-            return (arr[meioEsquerda] + arr[meio]) / 2;
-        } else {
-            return arr[meio];
-        }
-    };
-
-    // Verifica se o comprimento total é par ou ímpar
-    const comprimentoTotal = nums1.length + nums2.length;
-    const éPar = comprimentoTotal % 2 === 0;
-
-    // Chama a função recursiva para encontrar a mediana
-    if (éPar) {
-        return encontrarMedianaRecursivamente(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1);
+    if (tamanhoTotal % 2 === 1) {
+        // Se o tamanho total for ímpar, retorna o valor do meio
+        return encontrarElementoK(nums1, nums2, Math.floor(tamanhoTotal / 2) + 1);
     } else {
-        const meio = Math.floor(comprimentoTotal / 2);
-        return encontrarMedianaRecursivamente(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1);
+        // Se o tamanho total for par, retorna a média dos dois valores do meio
+        const esquerda = encontrarElementoK(nums1, nums2, tamanhoTotal / 2);
+        const direita = encontrarElementoK(nums1, nums2, tamanhoTotal / 2 + 1);
+        return (esquerda + direita) / 2;
     }
 }
+
+function encontrarElementoK(nums1: number[], nums2: number[], k: number): number {
+    const len1 = nums1.length;
+    const len2 = nums2.length;
+
+    if (len1 > len2) {
+        // Garantimos que nums1 seja sempre o array menor
+        return encontrarElementoK(nums2, nums1, k);
+    }
+
+    if (len1 === 0) {
+        // Se o primeiro array estiver vazio, retorna o k-ésimo elemento do segundo array
+        return nums2[k - 1];
+    }
+
+    if (k === 1) {
+        // Retorna o menor elemento entre os dois arrays
+        return Math.min(nums1[0], nums2[0]);
+    }
+
+    const indice1 = Math.min(Math.floor(k / 2), len1);
+    const indice2 = k - indice1;
+
+    if (nums1[indice1 - 1] < nums2[indice2 - 1]) {
+        // Descarta a parte inferior de nums1 e continua procurando em nums2
+        return encontrarElementoK(nums1.slice(indice1), nums2, k - indice1);
+    } else {
+        // Descarta a parte inferior de nums2 e continua procurando em nums1
+        return encontrarElementoK(nums1, nums2.slice(indice2), k - indice2);
+    }
+}
+
+
